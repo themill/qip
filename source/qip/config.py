013322fcb785258020c89a808473e167073d42b1
Sven Steinbauer <sven@themill.com>
2018-05-24 14:10:25 +0100

    Strip flask docs from config parser
    The whole system relies on `pip` to do the work. All dependencies and
    operations are resolved through subprocess.
    
    Packages are requested to be installed and qip will fetch the
    dependencies and then install those one by one into the index, and
    finally install the final package.
    
    If a package is missing from the index the user will be prompted to
    download it. If the user says no, the install continues with the next
    dependency.

--- source/qip/config.py
+++ source/qip/config.py
@@ -3,52 +3,11 @@ import types
 import errno
 
 class Config(dict):
-    """Works exactly like a dict but provides ways to fill it from files
-    or special dictionaries.  There are two common patterns to populate the
-    config.
-    Either you can fill the config from a config file::
-        app.config.from_pyfile('yourconfig.cfg')
-    Or alternatively you can define the configuration options in the
-    module that calls :meth:`from_object` or provide an import path to
-    a module that should be loaded.  It is also possible to tell it to
-    use the same module and with that provide the configuration values
-    just before the call::
-        DEBUG = True
-        SECRET_KEY = 'development key'
-        app.config.from_object(__name__)
-    In both cases (loading from any Python file or loading from modules),
-    only uppercase keys are added to the config.  This makes it possible to use
-    lowercase values in the config file for temporary values that are not added
-    to the config or to define the config keys in the same file that implements
-    the application.
-    Probably the most interesting way to load configurations is from an
-    environment variable pointing to a file::
-        app.config.from_envvar('YOURAPPLICATION_SETTINGS')
-    In this case before launching the application you have to set this
-    environment variable to the file you want to use.  On Linux and OS X
-    use the export statement::
-        export YOURAPPLICATION_SETTINGS='/path/to/config/file'
-    On windows use `set` instead.
-    :param root_path: path to which files are read relative from.  When the
-                      config object is created by the application, this is
-                      the application's :attr:`~flask.Flask.root_path`.
-    :param defaults: an optional dictionary of default values
-    """
-
     def __init__(self, defaults=None):
         dict.__init__(self, defaults or {})
         self.root_path = os.path.dirname(__file__)
 
     def from_envvar(self, variable_name, silent=False):
-        """Loads a configuration from an environment variable pointing to
-        a configuration file.  This is basically just a shortcut with nicer
-        error messages for this line of code::
-            app.config.from_pyfile(os.environ['YOURAPPLICATION_SETTINGS'])
-        :param variable_name: name of the environment variable
-        :param silent: set to ``True`` if you want silent failure for missing
-                       files.
-        :return: bool. ``True`` if able to load config, ``False`` otherwise.
-        """
         rv = os.environ.get(variable_name)
         if not rv:
             if silent:
@@ -61,17 +20,6 @@ class Config(dict):
         return self.from_pyfile(rv, silent=silent)
 
     def from_pyfile(self, filename, silent=False):
-        """Updates the values in the config from a Python file.  This function
-        behaves as if the file was imported as module with the
-        :meth:`from_object` function.
-        :param filename: the filename of the config.  This can either be an
-                         absolute filename or a filename relative to the
-                         root path.
-        :param silent: set to ``True`` if you want silent failure for missing
-                       files.
-        .. versionadded:: 0.7
-           `silent` parameter.
-        """
         filename = os.path.join(self.root_path, filename)
         d = types.ModuleType('config')
         d.__file__ = filename
@@ -89,26 +37,6 @@ class Config(dict):
         return True
 
     def from_object(self, obj):
-        """Updates the values from the given object.  An object can be of one
-        of the following two types:
-        -   a string: in this case the object with that name will be imported
-        -   an actual object reference: that object is used directly
-        Objects are usually either modules or classes. :meth:`from_object`
-        loads only the uppercase attributes of the module/class. A ``dict``
-        object will not work with :meth:`from_object` because the keys of a
-        ``dict`` are not attributes of the ``dict`` class.
-        Example of module-based configuration::
-            app.config.from_object('yourapplication.default_config')
-            from yourapplication import default_config
-            app.config.from_object(default_config)
-        You should not use this function to load the actual configuration but
-        rather configuration defaults.  The actual config should be loaded
-        with :meth:`from_pyfile` and ideally from a location not within the
-        package because the package might be installed system wide.
-        See :ref:`config-dev-prod` for an example of class-based configuration
-        using :meth:`from_object`.
-        :param obj: an import name or object
-        """
         for key in dir(obj):
             if key.isupper():
                 self[key] = getattr(obj, key)
