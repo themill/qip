c4b55136580c12b31d9a89433048606d757430b7
Claudia Zimmer <claudiaz@themill.com>
2018-05-25 17:09:52 -0700

    Start unit tests. Add fixture to set config file (override whatever is the shell at the time). Add test for no arguments.

--- test/unit/test_command_line.py
+++ test/unit/test_command_line.py
@@ -1,8 +1,29 @@
 # :coding: utf-8
 
+import os
+import pytest
+
+import qip
 from qip.command_line import main
 
 
-def test_with_defaults():
-    """Command executes successfully with defaults."""
-    assert main([]) is None
+@pytest.fixture()
+def set_development_environment_variable(mocker):
+    """Mock the environment variable used by config.
+
+    Ensure that the environment variable CONFIG is explicitly set
+    to config/base.py for the entirety of the module using the fixture.
+
+    """
+    mocker.patch.dict(os.environ, {
+        "QIP_CONFIG": os.path.realpath(os.path.join(
+            os.path.dirname(__file__), "../../source/qip/config/base.py"))
+    })
+
+
+def test_without_arguments():
+    """Command without arguments."""
+    with pytest.raises(SystemExit) as raised:
+        qip.command_line.main(arguments=None)
+
+    assert raised.value.code == 0
--- test/unit/test_qip.py
+++
@@ -1,8 +0,0 @@
-# :coding: utf-8
-
-import qip
-
-
-def test_something():
-    """Something should work!"""
-    assert qip.fix_me is True
