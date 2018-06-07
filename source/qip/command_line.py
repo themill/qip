56a700f400ec1539426a8d513a77da770229d5af
Sven Steinbauer <sven@themill.com>
2018-06-07 16:22:24 +0100

    Seems to be working with shadow so far

--- source/qip/command_line.py
+++ source/qip/command_line.py
@@ -8,6 +8,7 @@ import os
 import sys
 import tempfile
 import shutil
+import json
 
 import config
 from pkg_resources import Requirement as Req
@@ -42,7 +43,7 @@ def set_git_ssh(package):
 
 
 def has_git_version(package):
-    m = re.search(r'@.+$', package)
+    m = re.search(r'\.git@.+$', package)
     if m is None:
         return False
     return True
@@ -57,20 +58,20 @@ def fetch_dependencies(ctx, package, deps_install):
     output, _ = run_pip_command(cmd)
     deps = output[0].split()
 
-    ctx.logger.info("\tDeps resolved: {}".format(deps))
+    #ctx.logger.info("\tDeps resolved: {}".format(deps))
 
     for dep in deps:
         pkg_req = Req.parse(dep)
         name = pkg_req.unsafe_name
         specs = pkg_req.specs
         if name in deps_install.keys():
-            ctx.logger.info("\t{} already processed. Skipping.".format(name))
+            ctx.logger.info("\tSkipping {}. Already processed. ".format(name))
             continue
         deps_install[name] = specs
         fetch_dependencies(ctx, dep, deps_install)
 
 
-def is_package_installed(ctx, package):
+def is_installed(package, version):
     if os.path.exists("{1}/{0}".format(package, cfg["INSTALL_DIR"])):
         ctx.logger.info("{} already installed".format(package))
         return True
@@ -79,129 +80,92 @@ def is_package_installed(ctx, package):
 
 
 def run_pip_command(cmd):
-    print "RUNNING: ", cmd
+    #print "RUNNING: ", cmd
     ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = ps.communicate()
     return output, ps.returncode
 
 
-def check_to_download(ctx, package, output):
+def write_deps_to_file(name, specs, deps, filename):
+    with open(filename, 'w') as fh:
+        json.dump(deps, fh)
+
+
+def read_deps_from_file(name, specs, filename):
+    with open(filename, 'r') as fh:
+        deps = json.load(fh)
+    return deps
+
+
+def check_to_download(ctx, package, spec, output):
     if output[1].split('\n')[-2].startswith("No matching distribution found for"):
         ctx.logger.warning("{0} not found in package index.".format(package))
-        if click.confirm('Do you want to try and download it now?'):
-            if not download_package(ctx, package):
-                return False
-            install_package(ctx, package)
-            return True
-
+        return click.confirm('Do you want to try and download it now?')
     return False
 
 
-def download_package(ctx, package):
-    cmd = "pip download --no-deps --exists-action w --dest {0} --find-links {0}".format(cfg["PACKAGE_INDEX"])
-    to_download = set_git_ssh(package)
+def download_package(ctx, package, spec):
+    cmd = ("pip download --no-deps --exists-action w "
+           "--dest {0} --no-cache --find-links {0}".format(cfg["PACKAGE_INDEX"])
+          )
 
-    cmd += " '{}'".format(to_download)
-    ctx.logger.info("Downloading {0}".format(to_download))
+    if not spec:
+        spec = ''
+    cmd += " '{}{}'".format(package, spec)
+    ctx.logger.info("Downloading {0} {1}".format(package, spec))
     output, ret_code = run_pip_command(cmd)
 
     if ret_code != 0:
         ctx.logger.error("Unable to download requested package. Reason from pip below")
         ctx.logger.error(output[1])
+        ctx.logger.warning("If this is a package from Gitlab you should download it first.")
         return False
 
     return True
 
 
-def install_cmd_gitlab(ctx, package, target_dir):
-    package = set_git_ssh(package)
-    if not has_git_version(package):
-        ctx.logger.error("Please specify a version with `@` when installing from git")
+def install_package(ctx, package, version, download=False):
+    spec = ','.join( (ver[0] + ver[1] for ver in version) )
+    ctx.logger.info("Installing {} : {}".format(package, spec))
+
+    try:
+        temp_dir = tempfile.mkdtemp(dir=cfg["INSTALL_DIR"])
+    except OSError:
+        ctx.logger.error("Unable to create temp directory")
         sys.exit(1)
 
-    package_name = os.path.basename(package)
-    package_name, version = package_name.split('.git@')
-    cmd = " '{1}' '{0}'".format(package, target_dir)
-    return cmd
+    cmd = ("pip install --ignore-installed --no-deps --prefix {0}"
+           " --no-index --no-cache-dir --find-links {1}"
+           " '{2}{3}'".format(temp_dir, cfg["PACKAGE_INDEX"], package, spec)
+          )
 
-
-def install_cmd_pypi(ctx, package, target_dir):
-    m = re.search(r"(.*)[><=]+([\d\.]+)", package)
-
-    pkg_req = Req.parse(package)
-    name = pkg_req.unsafe_name
-    specs = pkg_req.specs
-
-    cmd = ""
-    # if package does not end with number, get version
-    if not specs:
-        test_cmd = "pip install --ignore-installed '{}=='".format(package)
-        output, ret_code = run_pip_command(test_cmd)
-
-        # This is expected to fail to give us a list of available versions
-        match = re.search(r"\(from versions: ((.*))\)", output[1])
-        if match:
-            latest_version = match.group(1).split(", ")[-1]
-        else:
-            if not check_to_download(ctx, package, output):
-                return
-
-        if is_package_installed(ctx, '{0}-{1}'.format(package, latest_version)):
-            return
-
-
-        cmd = " '{2}' '{0}=={1}'".format(package, latest_version, target_dir)
-
-    else:
-        if is_package_installed(ctx, '{0}-{1}'.format(m.group(1), m.group(2))):
-            return
-        #Installing html2text<2016.5,>=2016.4.2
-        cmd = " '{1}' '{0}'".format(name, target_dir)
-
-    return cmd
-
-
-def install_package(ctx, package):
-    ctx.logger.info("Installing {} ".format(package))
-
-    cmd = "pip install --ignore-installed --no-deps --prefix"
-    temp_dir = tempfile.mkdtemp(dir=cfg["INSTALL_DIR"])
-    if package.startswith("git@gitlab"):
-        tmp_cmd = install_cmd_gitlab(ctx, package, temp_dir)
-        if tmp_cmd is None:
-            return
-        cmd += tmp_cmd
-    else:
-        tmp_cmd = install_cmd_pypi(ctx, package, temp_dir)
-        if tmp_cmd is None:
-            return
-        cmd += tmp_cmd
-
-    cmd += " --no-index --no-cache-dir --find-links {0}".format(cfg["PACKAGE_INDEX"])
     output, ret_code = run_pip_command(cmd)
-    if ret_code != 0:
-        if not check_to_download(ctx, package, output):
-            match = re.search(r"\(from versions: ((.*))\)", output[1])
-            if not match:
-                ctx.logger.error(output[1])
+    if ret_code == 1:
+        if not download and not check_to_download(ctx, package, spec, output):
+            ctx.logger.warning("Not downloading {}. Skipping installation.".format(package))
+            shutil.rmtree(temp_dir)
+        else:
+            shutil.rmtree(temp_dir)
+            if not download_package(ctx, package, spec):
+                return
+            install_package(ctx, package, version)
     else:
         lastline = output[0].split('\n')[-2].strip()
         m = re.search(r'(\S+-[\d\.]+)$', lastline)
         if m:
-            # move directory
-            os.rename(temp_dir, "{0}/{1}".format(cfg["INSTALL_DIR"], m.group(1)))
-            #shutil.rmtree(temp_dir)
+            try:
+                os.rename(temp_dir, "{0}/{1}".format(cfg["INSTALL_DIR"], m.group(1)))
+            except OSError:
+                shutil.rmtree(temp_dir)
 
 
 @qipcmd.command()
 @click.pass_obj
 @click.argument('package')
+@click.option('--nodeps', is_flag=True, help='Just install the specified package without deps')
+@click.option('--download', is_flag=True, help='Download packages without prompting')
 def install(ctx, **kwargs):
     """Install PACKAGE to its own subdirectory under the configured target directory"""
-
-    ctx.logger.info("Fetching deps for {} and all its deps. This may take some time.".format(kwargs['package']))
-
-    deps = {}
 
     package = set_git_ssh(kwargs['package'])
     if package.startswith("git+ssh://"):
@@ -216,13 +180,38 @@ def install(ctx, **kwargs):
         name = pkg_req.unsafe_name
         specs = pkg_req.specs
 
+    version = '_'.join( (ver[0] + ver[1] for ver in specs) )
+    filename = os.path.join(cfg["DEP_STORE"], "{}-{}".format(name, version))
+    has_dep_file = False
+
+    if not kwargs['nodeps']:
+        if os.path.isfile(filename):
+            ctx.logger.info("Found a deps file for this package.")
+            if click.confirm('Read deps from it?'):
+                ctx.logger.info("Reading deps from file {}.".format(filename))
+                deps = read_deps_from_file(name, specs, filename)
+                has_dep_file = True
+            else:
+                deps = {}
+                ctx.logger.info("Fetching deps for {} and all its deps. This may take some time.".format(kwargs['package']))
+                fetch_dependencies(ctx, package, deps)
+        else:
+            deps = {}
+            ctx.logger.info("Fetching deps for {} and all its deps. This may take some time.".format(kwargs['package']))
+            fetch_dependencies(ctx, package, deps)
+
     deps[name] = specs
 
-    fetch_dependencies(ctx, package, deps)
-    print deps
+    ctx.logger.info("Dependencies resolved. Required packages:")
+    ctx.logger.info("\t{}".format(', '.join(deps.keys())))
+    if not click.confirm('Do you want to continue?'):
+        sys.exit(0)
+    if not has_dep_file:
+        write_deps_to_file(name, specs, deps, filename)
+
     for package, version in deps.iteritems():
-        ctx.logger.info("Installing {} : {}".format(package, version))
-        #install_package(package, version)
+        #ctx.logger.info("Installing {} : {}".format(package, version))
+        install_package(ctx, package, version, kwargs['download'])
     # if deps:
     #     ctx.logger.info("Installing deps as needed.")
     #     for dep in deps:
@@ -239,7 +228,13 @@ def install(ctx, **kwargs):
 @click.argument('package')
 def download(ctx, **kwargs):
     """Download PACKAGE to its own subdirectory under the configured target directory"""
-    download_package(ctx, kwargs['package'])
+    if not has_git_version(kwargs['package']):
+        ctx.logger.error("Please specify a version with `@` when installing from git")
+        sys.exit(1)
+
+    package_name = set_git_ssh(kwargs['package'])
+    # Specs are already part of the package_name
+    download_package(ctx, package_name, None)
 
 
 def main(arguments=None):
--- source/qip/configs/base.py
+++ source/qip/configs/base.py
@@ -1,3 +1,4 @@
 INSTALL_DIR = "/tmp/qip-test/"
 #PACKAGE_INDEX = "/mill3d/server/apps/PYTHON/package-index/"
 PACKAGE_INDEX = "/tmp/my_packages/"
+DEP_STORE = "/tmp/qip_deps/"
