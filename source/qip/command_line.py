86e238d3f8048743799b4d9b12691b0272bd4ab8
Sven Steinbauer <sven@themill.com>
2018-07-06 13:28:54 +0100

    Fix init of CmdRunner

--- source/qip/command_line.py
+++ source/qip/command_line.py
@@ -144,7 +144,7 @@ def download(ctx, **kwargs):
 
     package_name = set_git_ssh(kwargs['package'])
     ctx.target = cfg["TARGETS"][ctx.target]
-    pip_run = CmdRunner(ctx, ctx.target, ctx.password)
+    pip_run = CmdRunner(ctx)
 
     qip = Qip(ctx, pip_run)
     # Specs are already part of the package_name in this case
