74b9b0c6fdf76161ee18a618e48af08c6e5d59dc
Sven Steinbauer <sven@themill.com>
2018-08-07 11:00:34 +0100

    ndle rsync error
    if the rsync exit status is not 0 then print the stderr output, but
    carry on  with the other servers

--- source/qip/cmdrunner.py
+++ source/qip/cmdrunner.py
@@ -58,7 +58,11 @@ class Command(object):
     def install_and_sync(self, from_dir, to_dir):
         for loc, server in self.ctx.cfg['LOCATION_LUT'].iteritems():
             cmd = "rsync -azvl {0}/ {2}:{1}".format(from_dir, to_dir, server)
-            self.run_cmd(cmd)
+            stdout, stderr, exit_status = self.run_cmd(cmd)
+            if exit_status != 0:
+                self.ctx.mlogger.error("Sync to {} failed with error: {}\n"
+                                       "Carrying on with other servers."
+                                       .format(server, stderr))
 
     def rmtree(self, path):
         cmd = "rm -rf {}".format(path)
