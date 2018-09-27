fb1d942ce5d6e50f190d11b1186f8a7a88bf13f7
jeremyr <jeremyr@themill.com>
2018-09-26 17:09:24 -0700

    Add module to deal with the execution of a command

---
+++ source/qip/command.py
@@ -0,0 +1,49 @@
+# :coding: utf-8
+
+from __future__ import print_function
+import subprocess
+import shlex
+
+import mlog
+
+
+def execute(command, environ_mapping, quiet=False):
+    """Execute *command* within *environ_mapping*.
+
+    * *command* should be in the form of::
+
+        "pip install foo"
+
+    * *environ_mapping* should be a mapping with all environment variables which
+    must be set for the *command* to run properly.
+
+    * *quiet* indicate whether output lines should be hidden. Default is False.
+
+    """
+    logger = mlog.Logger(__name__ + ".execute")
+    logger.debug(command)
+
+    process = subprocess.Popen(
+        shlex.split(command),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        env=environ_mapping
+    )
+
+    output = None
+
+    if not quiet:
+        output = []
+
+        lines_iterator = iter(process.stdout.readline, b"")
+        while process.poll() is None:
+            for line in lines_iterator:
+                output.append(line)
+                _line = line.rstrip()
+                print(_line.decode("latin"), end="\r\n")
+
+    errors = process.stderr.readlines()
+    if len(errors):
+        raise RuntimeError("".join(errors))
+
+    return "".join(output or process.stdout.readlines())
