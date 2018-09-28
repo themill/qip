d1f574c8e22590516e79d9dc730001014d3c9140
jeremyr <jeremyr@themill.com>
2018-09-27 18:02:47 -0700

    Extract description and platform from fetched metadata

--- source/qip/package.py
+++ source/qip/package.py
@@ -1,18 +1,20 @@
 # :coding: utf-8
 
 from __future__ import print_function
+import re
 import json
 
 import mlog
 
 import qip.command
+import qip.system
 
 
 def install(requirement, destination, environ_mapping):
     """Install package in *destination* from *requirement*.
 
     Return a mapping with information about the package, as returned by
-    :func:`fetch_package_from_environ`.
+    :func:`fetch_mapping_from_environ`.
 
     * *requirement* is an instance of
     :class:`packaging.requirements.Requirement`.
@@ -40,18 +42,79 @@ def install(requirement, destination, environ_mapping):
         environ_mapping
     )
 
-    return fetch_package_from_environ(requirement.name, environ_mapping)
+    return fetch_mapping_from_environ(requirement.name, environ_mapping)
 
 
-def fetch_package_from_environ(name, environ_mapping):
+def fetch_mapping_from_environ(name, environ_mapping):
     """Return package *name* from current environment.
 
     Return a mapping with information about the package. The mapping should be
     in the form of::
 
         {
+            "identifier": "Foo-0.1.0",
+            "name": "Foo",
+            "version": "0.1.0",
+            "description": "This is a Python package",
+            "system": {
+                "platform": "linux",
+                "arch": "x86_64",
+                "os": {
+                    "name": "centos",
+                    "major_version": 7
+                }
+            },
+            "requirements": [
+                {
+                    "identifier": "Bar-0.1.0",
+                    "request": "bar",
+                },
+                {
+                    "identifier": "Bim-2.3.1",
+                    "request": "bim >= 2, <3",
+                }
+            ]
+        }
+
+    * *environ_mapping* should be a mapping with all environment variables
+    needed.
+
+    """
+    logger = mlog.Logger(__name__ + ".fetch_package_from_environ")
+
+    dependency_mapping = extract_dependency_mapping(name, environ_mapping)
+    metadata_mapping = extract_metadata_mapping(name, environ_mapping)
+
+    mapping = {
+        "identifier": extract_identifier(dependency_mapping["package"]),
+        "name": dependency_mapping["package"]["package_name"],
+        "version": dependency_mapping["package"]["installed_version"],
+    }
+
+    mapping.update(metadata_mapping)
+
+    if len(dependency_mapping.get("dependencies", [])) > 0:
+        mapping["requirements"] = [
+            {
+                "identifier": extract_identifier(_dependency_mapping),
+                "request": extract_request(_dependency_mapping),
+            }
+            for _dependency_mapping in dependency_mapping["dependencies"]
+        ]
+
+    logger.info("Fetched {}.".format(mapping["identifier"]))
+    return mapping
+
+
+def extract_dependency_mapping(name, environ_mapping):
+    """Return package mapping for *name* from dependency mapping.
+
+    Return None if the package *name* cannot be found in dependency mapping.
+
+    A valid mapping should be in the form of::
+
+        {
             "package": {
-                "identifier": "Foo-0.1.0",
                 "key": "foo",
                 "package_name": "Foo",
                 "installed_version": "0.1.0",
@@ -76,8 +139,6 @@ def fetch_package_from_environ(name, environ_mapping):
     needed.
 
     """
-    logger = mlog.Logger(__name__ + ".fetch_package_from_environ")
-
     result = qip.command.execute(
         "pipdeptree --json", environ_mapping, quiet=True
     )
@@ -101,10 +162,45 @@ def fetch_package_from_environ(name, environ_mapping):
             "Impossible to fetch installed package for '{}'".format(name)
         )
 
-    # Build a unique identifier for the package.
-    mapping["package"]["identifier"] = extract_identifier(mapping["package"])
+    return mapping
 
-    logger.info("Fetched {}.".format(mapping["package"]["identifier"]))
+
+def extract_metadata_mapping(name, environ_mapping):
+    """Return package mapping for *name* from available metadata.
+
+    The mapping returned should be in the form of::
+
+        {
+            "description": "This is a Python package.",
+            "system": {
+                "platform": "linux",
+                "os": "el >= 6, <7"
+            }
+        }
+
+    * *environ_mapping* should be a mapping with all environment variables
+    needed.
+
+    """
+    result = qip.command.execute(
+        "pip show '{}' -v".format(name), environ_mapping, quiet=True
+    )
+
+    mapping = {}
+
+    match_description = re.search("(?<=Summary: ).+", result)
+    if match_description is not None:
+        mapping["description"] = match_description.group().strip()
+
+    # Find out if the package is platform specific from the classifiers
+    # (https://pypi.org/classifiers/)
+    os_classifiers = re.findall("Operating System :: .*", result)
+    if (
+        len(os_classifiers) > 1 and
+        os_classifiers[0] != "Operating System :: OS Independent"
+    ):
+        mapping["system"] = qip.system.query()
+
     return mapping
 
 
