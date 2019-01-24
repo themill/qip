2d4a863dffb78cdaff8460084a88ec81b8a05e17
jeremyr <jeremyr@themill.com>
2019-01-23 17:36:52 -0800

    Improve formatting

--- source/qip/package.py
+++ source/qip/package.py
@@ -239,7 +239,8 @@ def extract_metadata_mapping(name, environ_mapping):
 
     """
     result = qip.command.execute(
-        "pip show --disable-pip-version-check "
+        "pip show "
+        "--disable-pip-version-check "
         "'{}' -v".format(name), environ_mapping, quiet=True
     )
 
