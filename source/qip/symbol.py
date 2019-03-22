28a7934eead2cccfc5bfcc4a4067c2b2f80e2735
jeremyr <jeremyr@themill.com>
2019-03-22 13:46:39 -0700

    Remove unused 'bin' symbol and update 'lib' symbol to make it adapt to the python version

--- source/qip/symbol.py
+++ source/qip/symbol.py
@@ -1,6 +1,16 @@
 # :coding: utf-8
 
 import os
+import sys
+
+
+def _lib_destination():
+    """Return lib destination depending on the Python version."""
+    name = "python{major}.{minor}".format(
+        major=sys.version_info.major,
+        minor=sys.version_info.minor,
+    )
+    return os.path.join("lib", name, "site-packages")
 
 
 #: Variable symbol for :term:`Wiz` definition environment.
@@ -12,8 +22,5 @@ INSTALL_ROOT = "${INSTALL_ROOT}"
 #: :term:`Wiz` request to resolve :term:`Python` 2.7 environment.
 P27_REQUEST = "python==2.7.*"
 
-#: Relative path to installed libraries in :term:`Python` 2.7.
-P27_LIB_DESTINATION = os.path.join("lib", "python2.7", "site-packages")
-
-#: Relative path to installed executables.
-BIN_DESTINATION = "bin"
+#: Relative path to installed libraries in :term:`Python`.
+LIB_DESTINATION = _lib_destination
